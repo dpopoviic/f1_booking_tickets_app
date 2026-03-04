fddfc660f9a00a61d470b2381b2aa10a260d4999
Dragana Popović <dp20220322@student.fon.bg.ac.rs>
2026-03-04 19:29:13 +0100

    Set OpenApi and Scalar configuration

--- f1_booking_tickets_app_backend/f1_booking_tickets_API/Program.cs
+++ f1_booking_tickets_app_backend/f1_booking_tickets_API/Program.cs
@@ -1,22 +1,24 @@
 using f1_booking_tickets.DataAccess;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
